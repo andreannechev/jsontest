a730604a106bde630c5ad13f52b305fde7a0575a
Andrean Nechev <andrean.nechev@gmail.com>
2017-07-14 13:32:05 +0300

    single record fixed

--- app/ingrid/route.js
+++ app/ingrid/route.js
@@ -3,8 +3,19 @@ import Ember from 'ember';
 const { Route, set } = Ember;
 
 export default Route.extend({
+  // model() { //model hoock
+  //   return this.store.findAll('ingrid'); //name of the model
+  // }
+
   model() { //model hoock
-    return this.store.findAll('ingrid'); //name of the model
+    return this.store.queryRecord('ingrid', {
+      query: "lets go",
+      lang: "en",
+      sessionId: '1234567890'
+
+    }); //name of the model
   }
+
+
 
 });
